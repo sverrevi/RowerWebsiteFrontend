aaa37ace4ea1a72bb6983d44bc8852231eda70b6
Sverre Vinje <sverrevinje@gmail.com>
2023-10-02 12:13:12 +0200

    Very slightly change to the home page

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const HomeContent = () => {
   return (
     <div>
@@ -8,7 +6,7 @@ const HomeContent = () => {
         Embark on a journey with us as we delve into the world of rowing excellence. Whether you're a passionate rowing enthusiast or just curious to explore this dynamic sport, you've landed at the right place.
       </p>
       <p>
-        On this platform, we proudly showcase a diverse array of talented rowers and vibrant rowing clubs in your local region...
+        On this platform, we proudly showcase a diverse array of talented rowers and vibrant rowing clubs in your local region.
         {/* Add the rest of your introduction here */}
       </p>
     </div>
